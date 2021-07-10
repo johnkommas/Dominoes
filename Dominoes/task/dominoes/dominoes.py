c53989bea3f5de70c4be4c9249edbc4300234f0b
Ioannis E. Kommas <johnkommas@gmail.com>
2021-07-10 15:56:55 +0300

    k

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -32,6 +32,70 @@ def highest(pieces):
         return [0, 0]
 
 
+def output_results():
+    print('======================================================================')
+    print(f'Stock size: {len(stock)}')
+    print(f'Computer pieces: {len(computer)}')
+    print()
+    if len(snake) <= 6:
+        for i in snake:
+            print(i, end="")
+    else:
+        for i in snake[:3]:
+            print(i, end="")
+        print("...", end="")
+        for i in snake[-3:]:
+            print(i, end="")
+    print()
+    print()
+    print('Your pieces:')
+    for a, b in enumerate(player):
+        print(f"{a+1}:{b}")
+    if status == 'player' and len(computer) > 0:
+        print("Status: It's your turn to make a move. Enter your command.")
+    elif status == 'player' and len(computer) == 0:
+        print("Status: The game is over. The computer won!")
+        return True
+    elif status == 'computer' and len(player) > 0:
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+        response = input()
+    elif status == 'computer' and len(player) == 0:
+        print("Status: The game is over. You won!")
+        return True
+    else:
+        print("Status: The game is over. It's a draw!")
+        return True
+
+
+def make_move(desicion, turn_player):
+
+    # Select a domino and place it on the left side of the snake.
+    val = turn_player[abs(desicion) - 1]
+    if desicion < 0:
+        snake.insert(0, val)
+        turn_player.remove(val)
+    # Select a domino and place it on the right side of the snake.
+    elif desicion > 0:
+        snake.append(val)
+        turn_player.remove(val)
+    # Take an extra piece from the stock (if it's not empty) and skip a turn.
+    else:
+        if stock:
+            turn_player.append(stock.pop())
+
+
+def check_input():
+    try:
+        my_turn = int(input())
+        if abs(my_turn) > len(player):
+            print("Invalid input. Please try again.")
+            return check_input()
+    except ValueError:
+        print("Invalid input. Please try again.")
+        return check_input()
+    return my_turn
+
+
 while True:
     computer = start_game()
     player = start_game()
@@ -53,42 +117,17 @@ while True:
     else:
         pass
 
-
-def output_results():
-    print('======================================================================')
-    print(f'Stock size: {len(stock)}')
-    print(f'Computer pieces: {len(computer)}')
-    if len(snake) <= 6:
-        for i in snake:
-            print(i, end="")
-    else:
-        for i in snake[:3]:
-            print(i, end="")
-        print("...", end="")
-        for i in snake[-3:]:
-            print(i, end="")
-    print()
-    print('Your pieces:')
-    for a, b in enumerate(player):
-        print(f"{a+1}:{b}")
+while True:
+    end_game = output_results()
+    if end_game:
+        break
     if status == 'player':
-        print("Status: It's your turn to make a move. Enter your command.")
-    else:
-        print("Status: Computer is about to make a move. Press Enter to continue...")
+        value = check_input()
+        make_move(value, player)
+        status = 'computer'
+    elif status == 'computer':
+        computer_range_number = range(-len(computer), len(computer))
+        value = random.choice(computer_range_number)
+        make_move(value, computer)
+        status = 'player'
 
-
-
-def make_move():
-    pass
-
-
-def check_input():
-    try:
-        my_turn = int(input())
-    except ValueError:
-        print("Invalid input. Please try again.")
-        return check_input()
-    return my_turn
-
-
-x = check_input()
